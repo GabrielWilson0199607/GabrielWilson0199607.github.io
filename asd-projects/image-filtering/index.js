19afad7e28da110690ac95a156c8d3b0f5a69910
GabrielWilson0199607 <gcw0199607@lpssonline.com>
2023-12-13 02:56:59 +0000

    Saving image-filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -2,8 +2,15 @@
 // as soon as the page loads.
 $(document).ready(function () {
   render($("#display"), image);
-  $("#apply").on("click", applyAndRender);
-  $("#reset").on("click", resetAndRender);
+  $("#apply").on("click", function () {
+    applyFilter(reddify);
+    render($("#display"), image);
+  });
+
+
+  $("#reset").on("click", function () {
+    resetAndRender();
+  });
 });
 
 
@@ -39,32 +46,30 @@ function applyAndRender() {
 
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-function applyFilter() {
-  for (let r = 0; r < image.length; r++) {
-    for (let c = 0; c < image[r].length; c++) {
-      const rgbString = image[r][c];
-      const rgbNumbers = rgbStringToArray(rgbString);
-      rgbNumbers[RED] = 255;
-      const newColor = rgbArrayToString(rgbNumbers);
-      image[r][c] = newColor;
+function applyFilter(filterFunction) {
+  for (var r = 0; r < image.length; r++) {
+    for (var c = 0; c < image[r].length; c++) {
+      image[r][c] = filterFunction(image[r][c]);
     }
   }
 }
-applyFilter(reddify);
 
 
 // TODO 7: Create the applyFilterNoBackground function
 function applyFilterNoBackground() {
   const backgroundColor = image[0][0];
-  for (let r = 0; r < image.length; r++) {
-    for (let c = 0; c < image[r].length; c++) {
+  for (var r = 0; r < image.length; r++) {
+    for (var c = 0; c < image[r].length; c++) {
       if (image[r][c] !== backgroundColor) {
-        applyFilter(reddify);
+        image[r][c] = reddify(image[r][c]);
       }
     }
   }
 }
-applyFilterNoBackground();
+
+
+
+
 
 
 // TODO 5: Create the keepInBounds function
@@ -77,16 +82,21 @@ console.log(keepInBounds(127));
 
 
 // TODO 3: Create reddify function
-function reddify(array) {
-  array[RED] = 200;
+function reddify(rgbString) {
+  var rgbNumbers = rgbStringToArray(rgbString);
+  rgbNumbers[RED] = 200;
+  return rgbArrayToString(rgbNumbers);
 }
 // TODO 6: Create more filter functions
-function decreaseBlue(array) {
-  array[BLUE] = keepInBounds(array[BLUE] - 50);
+function decreaseBlue(rgbString) {
+  var rgbNumbers = rgbStringToArray(rgbString);
+  rgbNumbers[BLUE] = keepInBounds(rgbNumbers[BLUE] - 50);
+  return rgbArrayToString(rgbNumbers);
 }
-
-function increaseGreenByBlue(array) {
-  array[GREEN] = keepInBounds(array[GREEN] + array[BLUE]);
+function increaseGreenByBlue(rgbString) {
+  var rgbNumbers = rgbStringToArray(rgbString);
+  rgbNumbers[GREEN] = keepInBounds(rgbNumbers[GREEN] + rgbNumbers[BLUE]);
+  return rgbArrayToString(rgbNumbers);
 }
 applyFilter(reddify);
 applyFilter(decreaseBlue);
